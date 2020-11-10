750a81df74968ab3099545759e5aebb512b74911
hjspku <hjspku@gmail.com>
2020-11-10 14:58:17 +0800

    Change folder structure

--- provision.sh
+++ provision.sh
@@ -74,16 +74,14 @@ EOF
 
 cd /vagrant
 
-cd twitter
-
 python manage.py migrate
 
 # superuser名字
-USER="root"
+USER="admin"
 # superuser密码
-PASS="yourpassword"
+PASS="admin"
 # superuser邮箱
-MAIL="root@root.com"
+MAIL="admin@twitter.com"
 script="
 from django.contrib.auth.models import User;
 
@@ -91,11 +89,11 @@ username = '$USER';
 password = '$PASS';
 email = '$MAIL';
 
-if User.objects.filter(username=username).count()==0:
-       User.objects.create_superuser(username, email, password);
-       print('Superuser created.');
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created.');
 else:
-       print('Superuser creation skipped.');
+    print('Superuser creation skipped.');
 "
 printf "$script" | python manage.py shell
 
--- twitter/settings.py
+++ twitter/settings.py
@@ -74,10 +74,6 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'twitter',
