e5b56d97cfd19e0229ac52bc02a0beb1534fbe45
huan <huan@64deMacBook-Air.local>
2020-11-11 15:07:32 +0800

    api/

--- twitter/urls.py
+++ twitter/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers
-from accounts import views
+from api.accounts import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'api/users', views.UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
