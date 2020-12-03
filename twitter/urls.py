9325bb6bcae4850566bb03484ba59b2da6f3b9a2
hjspku <hjspku@gmail.com>
2020-12-03 16:07:08 +0800

    Support signin signup

--- twitter/urls.py
+++ twitter/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 
 router = routers.DefaultRouter()
 router.register(r'api/users', views.UserViewSet)
+router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
