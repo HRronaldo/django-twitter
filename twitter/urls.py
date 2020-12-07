f781d488ba592dfaa424ba9e7f29c97c6e8143f4
Devin Don <I.INF@Outlook.com>
2020-12-07 22:36:28 +0800

    feat(restful): add django rest framework

--- twitter/urls.py
+++ twitter/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
